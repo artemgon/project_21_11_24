c656946b391aedd48c8a9002fffde4e083df1c46
artemgon <artemhoncharuk123@gmail.com>
2025-01-18 13:20:01 +0200

    Task 1 is done.

--- Program.cs
+++ Program.cs
@@ -4,11 +4,25 @@ namespace project_21_11_24
 {
     internal class Program
     {
+        delegate void EvenNumber(int number);
         static void Main()
         {
             Console.ForegroundColor = ConsoleColor.DarkRed;
             try
             {
+                EvenNumber evenNumber = (number) =>
+                {
+                    if (number % 2 == 0)
+                    {
+                        Console.WriteLine($"{number} is an even number.");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{number} is an odd number.");
+                    }
+                };
+                evenNumber(5);
+                evenNumber(6);
             }
             catch (Exception e)
             {
