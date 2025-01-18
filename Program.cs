f9f7c727cd3976589e7465b36d2bfb14ebc61a0b
artemgon <artemhoncharuk123@gmail.com>
2025-01-18 13:28:30 +0200

    Task 5 is done.

--- Program.cs
+++ Program.cs
@@ -4,11 +4,26 @@ namespace project_21_11_24
 {
     internal class Program
     {
+        delegate int Max(int[] array);
         static void Main()
         {
             Console.ForegroundColor = ConsoleColor.DarkRed;
             try
             {
+                Max max = delegate (int[] array)
+                {
+                    int max = array[0];
+                    for (int i = 1; i < array.Length; i++)
+                    {
+                        if (array[i] > max)
+                        {
+                            max = array[i];
+                        }
+                    }
+                    return max;
+                };
+                int[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+                Console.WriteLine(max(array));
             }
             catch (Exception e)
             {
