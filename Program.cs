09027ad4dc89992965aa42434b2bf6a8d42447e4
artemgon <artemhoncharuk123@gmail.com>
2025-01-18 13:38:48 +0200

    Task 6 is done.

--- Program.cs
+++ Program.cs
@@ -9,16 +9,26 @@ namespace project_21_11_24
             Console.ForegroundColor = ConsoleColor.DarkRed;
             try
             {
-                var list = new List<int> { 1, 2, -1, -2, 4, 6, -11, 24, 99, -200, 11 };
-                int counter1 = 0, counter2 = 0;
-                list.ForEach(x => {
-                    if (x < 0)
-                        counter1++;
-                    if (list.Count(y => y == x) == 1)
-                        counter2++;
-                });
-                Console.WriteLine($"Quantity of negative numbers: {counter1}");
-                Console.WriteLine($"Quantity of unique numbers: {counter2}");
+                Console.WriteLine("Enter some text: ");
+                string text = Console.ReadLine() ?? "";
+                Console.WriteLine("Enter the word you want to find: ");
+                string word = Console.ReadLine() ?? "";
+                Func<string, string, string> checkWord = (txt, wrd) =>
+                {
+                    var words = txt.Split(' ');
+                    for (int i = 0; i < words.Length; i++)
+                    {
+                        if (words[i] == wrd)
+                        {
+                            string leftWord = i > 0 ? words[i - 1] : "No left word";
+                            string rightWord = i < words.Length - 1 ? words[i + 1] : "No right word";
+                            return $"Yeah, the word is found. Left word: {leftWord}, Right word: {rightWord}";
+                        }
+                    }
+                    return "No, the word is not found.";
+                };
+                string result = checkWord(text, word);
+                Console.WriteLine(result);
             }
             catch (Exception e)
             {
