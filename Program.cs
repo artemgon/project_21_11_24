9473b8962923a36e2df1d448475f17d33b629208
artemgon <artemhoncharuk123@gmail.com>
2025-01-18 13:29:15 +0200

    Task 6 is done.

--- Program.cs
+++ Program.cs
@@ -4,11 +4,26 @@ namespace project_21_11_24
 {
     internal class Program
     {
+        delegate int Min(int[] array);
         static void Main()
         {
             Console.ForegroundColor = ConsoleColor.DarkRed;
             try
             {
+                Min min = delegate (int[] array)
+                {
+                    int min = array[0];
+                    for (int i = 1; i < array.Length; i++)
+                    {
+                        if (array[i] < min)
+                        {
+                            min = array[i];
+                        }
+                    }
+                    return min;
+                };
+                int[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+                Console.WriteLine(min(array));
             }
             catch (Exception e)
             {
