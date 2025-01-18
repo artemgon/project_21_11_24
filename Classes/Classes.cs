be5688970d86c2e82f3a96bd0b46a869ba4e8ef6
artemgon <artemhoncharuk123@gmail.com>
2025-01-18 13:17:57 +0200

    Add project files.

---
+++ Classes/Classes.cs
@@ -0,0 +1,7 @@
+﻿
+namespace project_21_11_24.Classes
+{
+    internal class Class
+    {
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using project_21_11_24.Classes;
+
+namespace project_21_11_24
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            try
+            {
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
