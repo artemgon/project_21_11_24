3a900ea481bef003c20d4a1f7f148cc1a20779e1
artemgon <artemhoncharuk123@gmail.com>
2025-01-18 13:22:28 +0200

    Task 2 is done

--- Program.cs
+++ Program.cs
@@ -4,11 +4,17 @@ namespace project_21_11_24
 {
     internal class Program
     {
+        delegate int Square(int x);
         static void Main()
         {
             Console.ForegroundColor = ConsoleColor.DarkRed;
             try
             {
+                Square square = delegate (int x)
+                {
+                    return x * x;
+                };
+                Console.WriteLine(square(5));
             }
             catch (Exception e)
             {
