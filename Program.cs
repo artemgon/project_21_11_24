9f3ee10a071e71eda101e34cdcf241eda64e9804
artemgon <artemhoncharuk123@gmail.com>
2025-01-18 13:23:38 +0200

    Task 3 is done.

--- Program.cs
+++ Program.cs
@@ -4,11 +4,17 @@ namespace project_21_11_24
 {
     internal class Program
     {
+        delegate int Cube(int x);
         static void Main()
         {
             Console.ForegroundColor = ConsoleColor.DarkRed;
             try
             {
+                Cube myNumber = delegate(int x)
+                {
+                    return x * x * x;
+                };
+                Console.WriteLine(myNumber(3));
             }
             catch (Exception e)
             {
