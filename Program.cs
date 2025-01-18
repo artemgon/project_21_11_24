e0062aab75d8482a25d7e867a8c7756cf0353192
artemgon <artemhoncharuk123@gmail.com>
2025-01-18 13:38:17 +0200

    Task 5 is done.

--- Program.cs
+++ Program.cs
@@ -9,6 +9,16 @@ namespace project_21_11_24
             Console.ForegroundColor = ConsoleColor.DarkRed;
             try
             {
+                var list = new List<int> { 1, 2, -1, -2, 4, 6, -11, 24, 99, -200, 11 };
+                int counter1 = 0, counter2 = 0;
+                list.ForEach(x => {
+                    if (x < 0)
+                        counter1++;
+                    if (list.Count(y => y == x) == 1)
+                        counter2++;
+                });
+                Console.WriteLine($"Quantity of negative numbers: {counter1}");
+                Console.WriteLine($"Quantity of unique numbers: {counter2}");
             }
             catch (Exception e)
             {
