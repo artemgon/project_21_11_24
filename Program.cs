220508800424c072aec2738fc616f5240a89d9ee
artemgon <artemhoncharuk123@gmail.com>
2025-01-18 13:31:29 +0200

    Task 7 is done.

--- Program.cs
+++ Program.cs
@@ -4,11 +4,26 @@ namespace project_21_11_24
 {
     internal class Program
     {
+        delegate void FindOdd(int[] arr);
         static void Main()
         {
             Console.ForegroundColor = ConsoleColor.DarkRed;
             try
             {
+                FindOdd findOdd = delegate (int[] arr)
+                {
+                    for (int i = 0; i < arr.Length; i++)
+                    {
+                        if (arr[i] % 2 != 0)
+                        {
+                            Console.Write("Odd number: ");
+                            Console.WriteLine();
+                            Console.WriteLine(arr[i]);
+                        }
+                    }
+                };
+                int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+                findOdd(arr);
             }
             catch (Exception e)
             {
