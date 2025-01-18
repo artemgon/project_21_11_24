b1ca22acf390c6daea4b11cb1ac3b4a1850eddb6
artemgon <artemhoncharuk123@gmail.com>
2025-01-18 13:37:38 +0200

    Task 4 is done.

--- Program.cs
+++ Program.cs
@@ -9,6 +9,15 @@ namespace project_21_11_24
             Console.ForegroundColor = ConsoleColor.DarkRed;
             try
             {
+                var list = new List<int> { 1, 2, -1, -2, 4, 6, -11, 24, 99, -200, 11 };
+                int counter = 0;
+                List<int> positiveNumbers = list.Where(x => x > 0).ToList();
+                foreach (var item in positiveNumbers)
+                {
+                    Console.WriteLine(item);
+                    counter++;
+                }
+                Console.WriteLine($"Count of positive numbers: {counter}");
             }
             catch (Exception e)
             {
